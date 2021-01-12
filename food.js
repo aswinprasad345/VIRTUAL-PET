81513f098d123589d1021e083af2140ba19841f8
aswinprasad345 <74087616+aswinprasad345@users.noreply.github.com>
2021-01-12 11:57:51 +0530

    Add files via upload

--- food.js
+++ food.js
@@ -1,6 +1,5 @@
 class Food {
     constructor(){
-        this.d
         this.lastFed = '';
         this.image = loadImage('images/Milk.png');
         this.width = width;
@@ -17,15 +16,15 @@ class Food {
 
     }
     readStock(data){
-        foodS = data.val();
+        this.foodS = data.val();
     }
     display(){
         var x = 80,y = 100
         push();
         imageMode(CENTER);
         image(this.image,720,220,70,70);
-        if(this.foodStock!=0){
-            for(var i = 0;this.foodstock;i++){
+        if(this.foodS!=0){
+            for(var i = 0;this.foodS;i++){
                 if(i%10==0){
                     x = 80
                     y = y + 50
